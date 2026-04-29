f7698370456adeb68ee9dddedb63671a4914fe8a
mounika <189mounika.b@gmail.com>
2026-04-29 21:40:33 +0530

    adding form

--- pro/app/views.py
+++ pro/app/views.py
@@ -13,3 +13,11 @@ def singledata(request,id):
 def mul_data(request):
     object=student.objects.all()
     return render(request,'mul_data.html',{'m':object})
+def form(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        age=request.POST.get('age')
+        subject=request.POST.get('subject')
+    return render(request,'forms.html')
+
+    
