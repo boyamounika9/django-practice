d305aaec7eed20a0f2536577868d60573c3744ea
mounika <189mounika.b@gmail.com>
2026-04-29 16:51:15 +0530

    showing multiple data

--- pro/app/views.py
+++ pro/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import student
+from .models import student
 
 # Create your views here.
 def mounika(request):
@@ -10,3 +10,6 @@ def home(request):
 def singledata(request):
     object=student.objects.get(id=1)
     return render(request,'singledata.html',{'s':object})
+def mul_data(request):
+    object=student.objects.all()
+    return render(request,'mul_data.html',{'m':object})
